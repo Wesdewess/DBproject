3a5d12cca0be228b3da8c1402e1a970e511841d2
Sti-jn <Stijn.d2000@Hotmail.com>
2018-05-18 12:45:11 +0200

    ff testen

--- DBapplication/src/dbapplication/DBapplication.java
+++ DBapplication/src/dbapplication/DBapplication.java
@@ -20,7 +20,6 @@ public class DBapplication {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("test");
-        hallo
     }
     
 }
