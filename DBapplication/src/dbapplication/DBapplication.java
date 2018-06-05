53ae971548c4c47b4079bccc84474d8f56afcb08
Sti-jn <Stijn.d2000@Hotmail.com>
2018-06-05 14:59:15 +0200

    (hopelijk) laatse verzie van de class Query
    De class query is volledig gemaakt en gecomment, de DB.properties file is nu ook af

--- DBapplication/src/dbapplication/DBapplication.java
+++ DBapplication/src/dbapplication/DBapplication.java
@@ -18,8 +18,7 @@ public class DBapplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("test");
+        
         Query.query();
     }
     
