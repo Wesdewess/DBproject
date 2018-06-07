48d2f6a4c1c51423e7f038539662efa2bc45bb6d
Sti-jn <Stijn.d2000@Hotmail.com>
2018-06-07 14:38:13 +0200

    .

--- DBapplication/src/dbapplication/DemoDB13.java
+++ DBapplication/src/dbapplication/DemoDB13.java
@@ -124,7 +124,7 @@ public class DemoDB13 {
                     }
                     
                     System.out.println("Selected row: " + tabel.getSelectedRow());
-                    System.out.println("Selected row: " + tabel.getSelectedColumn());
+                    System.out.println("Selected Column: " + tabel.getSelectedColumn());
                     System.out.println("Value at: " + tabel.getValueAt(row, column));
                     if(qryPrepStat != null){
                         break;
